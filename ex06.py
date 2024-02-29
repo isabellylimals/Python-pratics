f45b2f02cde70d50965ce238adfafcc5ca424d5c
Isabelly Lima <mslima5643@gmail.com>
2024-02-29 14:29:45 -0300

    new ex

---
+++ ex06.py
@@ -0,0 +1,6 @@
+
+import math
+#Double, triple, and square root of a number:
+number=int(input('Enter a number'))
+squareroot= math.sqrt(number)
+print('The double of {} is {} the triple is {} and the square root {}'.format(number, (number+number),(number+number+number), (squareroot)))
