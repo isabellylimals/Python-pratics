0bd04fb82f3d8679ad788868bcf0d042e260a3d6
Isabelly Lima <mslima5643@gmail.com>
2024-06-19 21:02:25 -0300

    up

--- manipulandotextos/ex02.py
+++ manipulandotextos/ex02.py
@@ -1,1 +1,21 @@
-numero= int(input)('Digite um numero de 1 a 9999:')
+# Solicita ao usuário que digite um número de 1 a 9999
+number = int(input('Enter a number from 1 to 9999:'))
+
+# Obtém o dígito das unidades
+unit = number // 1 % 10
+
+# Obtém o dígito das dezenas
+tens = number // 10 % 10
+
+# Obtém o dígito das centenas
+hundreds = number // 100 % 10
+
+# Obtém o dígito dos milhares
+thousands = number // 1000 % 10
+
+# Imprime os resultados
+print('Analyzing the number:')
+print('Unit: {}'.format(unit))
+print('Tens: {}'.format(tens))
+print('Hundreds: {}'.format(hundreds))
+print('Thousands: {}'.format(thousands))
---
+++ manipulandotextos/ex03.py
@@ -0,0 +1,5 @@
+#verificar se existe a letra 's' na primeira palavra
+cidade=str(input('Em que cidade voce nasceu?'))
+cidade.strip()
+print(cidade.count('S', 's'))
+print(cidade[:1]== 's' 'S')  
